517ded3050030320ff341a2c3e21af3829433439
Wellington Junior <wndrj7@gmail.com>
2021-03-26 00:36:46 -0300

    funções de volume, velocidade e tom

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,11 +1,11 @@
 //Inicializar o objeto SpeechSynthesisUtterance
-let speech = new SpeechSynthesisUtterance();
+var speech = new SpeechSynthesisUtterance();
 
 //Definindo o idioma
 speech.lang = "en";
 
 // array de vozes
-let voices = [];
+var voices = [];
 
 if('speechSynthesis' in window){
 
@@ -15,12 +15,12 @@ if('speechSynthesis' in window){
     let langRegex = /^en(-[a-z]{2})?$/i;
 
     // Obter lista de vozes inglês
-    let voices = synthesis.getVoices().filter(voice => langRegex.test(voice.lang));
+    voices = synthesis.getVoices().filter(voice => langRegex.test(voice.lang));
 
     //Defina inicialmente a primeira voz;
     speech.voice = voices[0];
 
-    //Defina a lista de seleção de voz. 
+    //Define a lista de seleção de voz. 
     let voiceSelect = document.querySelector("#voices");
 
     voices.forEach(function(voice, i){
@@ -32,24 +32,60 @@ else{
     console.error('Text-to-speech não suportado.');
 }
 
+document.querySelector("#rate").addEventListener("input", () => {
+    // obtendo o valor da velocidade
+    const rate = document.querySelector("#rate").value;
+  
+    // definindo velocidade
+    speech.rate = rate;
+  
+    // atualizar rotulo de velocidade
+    document.querySelector("#rate-label").innerHTML = rate;
+});
+
+document.querySelector("#volume").addEventListener("input", () => {
+    // obtendo o valor do volume
+    const volume = document.querySelector("#volume").value;
+  
+    // definindo volume
+    speech.volume = volume;
+  
+    // atualizar rotulo do volume
+    document.querySelector("#volume-label").innerHTML = volume;
+});
+
+document.querySelector("#pitch").addEventListener("input", () => {
+    // obtendo valor do tom
+    const pitch = document.querySelector("#pitch").value;
+  
+    // definindo tom
+    speech.pitch = pitch;
+  
+    // atualizar rotulo do tom
+    document.querySelector("#pitch-label").innerHTML = pitch;
+});
+
 // função de mudança de voz
 document.querySelector('#voices').addEventListener("change", ()=>{
     //console.log(document.querySelector("#voices").value);
     speech.voice = voices[document.querySelector("#voices").value];
 });
 
+// função de iniciar audio
 document.querySelector("#start").addEventListener("click", () => {
     // obtem o texto em inglês
     speech.text = document.querySelector("textarea").value;
   
     // inicia o audio
     window.speechSynthesis.speak(speech);
-  });
+});
 
-// speech.text = "Hello";
+//função de parar audio
+document.querySelector("#pause").addEventListener("click", () => {
+    window.speechSynthesis.pause();
+});
 
-// window.speechSynthesis.speak(speech);
-
-// document.querySelector("#talk").addEventListener("click", () => {
-//     speech.text = document.querySelector("textarea").value;
-// });
+//função de retomar
+document.querySelector("#resume").addEventListener("click", () => {
+    window.speechSynthesis.resume();
+});
